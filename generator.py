73783d0acb973331b4c4d610e5c0b220495e4275
bobromero <rromero123102@gmail.com>
2025-01-15 11:48:23 -0500

    create virtual enviornment

---
+++ generator.py
@@ -0,0 +1,5 @@
+#character generator
+
+#i want to get a random name and stats
+#the name can be anything, maybe from a library of names somewhere
+#the stats are going to be random with each one starting at 20 and ranging from 0-99
