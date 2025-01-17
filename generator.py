65c03d09c05b8276a4fca9e0301725623e9a352a
bobromero <rromero123102@gmail.com>
2025-01-17 13:42:19 -0500

    add random name generator

--- generator.py
+++ generator.py
@@ -4,9 +4,9 @@
 #the name can be anything, maybe from a library of names somewhere
 #the stats are going to be random with each one starting at 20 and ranging from 0-99
 import random
-
+from names_generator import generate_name
 statline = [20,20,20,20,20,20] # str,dex,health,confidence,int,charisma
-
+stats = ["Strength", "Dexterity", "Health", "Confidence", "Intelligence", "Charisma"]
 availablePoints = 20
 
 
@@ -17,13 +17,16 @@ def genCharacter():
             addPoints(random.randint(1,20),i)
         elif x < 0:
             removePoints(random.randint(1,20),i)
-    print("str,dex,health,confidence,int,charisma")
-    print(statline)
+
+        print(stats[i], ":\t", statline[i]) 
+    print("Name: ", generate_name())
 def addPoints(amount, stat):
     statline[stat] += amount
 
 def removePoints(amount, stat):
     statline[stat] -= amount
 
-
-genCharacter()
+ing = input()
+while ing != "e":
+    genCharacter()
+    ing = input()
